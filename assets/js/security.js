e878d9d9db5a916d9ece9a426052ae7221411730
twango <jamesding365@gmail.com>
2021-01-27 11:21:30 -0800

    minor security updates

--- assets/js/security.js
+++ assets/js/security.js
@@ -4,3 +4,20 @@ function encryptAndHash(string, key) {
   let hash = cryptoJS.SHA3(string) // SHA3 Hashing is used
   return cryptoJS.AES.encrypt(hash, key).toString()
 }
+
+function get(url) {
+  let request = new XMLHttpRequest()
+  request.onreadystatechange = function() {
+    if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+      return JSON.parse(request.responseText)
+    } else {
+      return null
+    }
+  }
+  request.open('GET', url, true)
+  request.send()
+}
+
+window.onload = async function onLoad() {
+
+}
