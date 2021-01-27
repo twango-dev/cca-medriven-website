4c60333a137b0ff7583dc1ace7d97120af6f0f22
twango <jamesding365@gmail.com>
2021-01-27 14:44:03 -0800

    encryption/decryption hashing page done

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,23 +1,46 @@
-const cryptoJS = window.cryptoJS
+const keysURL = 'https://twango-dev.github.io/cca-medriven-website/security/keys.json'
 
-function encryptAndHash(string, key) {
-  let hash = cryptoJS.SHA3(string) // SHA3 Hashing is used
-  return cryptoJS.AES.encrypt(hash, key).toString()
-}
-
-function get(url) {
+function get(url, callback) {
   let request = new XMLHttpRequest()
-  request.onreadystatechange = function() {
-    if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
-      return JSON.parse(request.responseText)
-    } else {
-      return null
-    }
-  }
   request.open('GET', url, true)
   request.send()
+  request.onreadystatechange = function() {
+    if (request.readyState == XMLHttpRequest.DONE) {
+      callback(request)
+    }
+  }
 }
 
-window.onload = async function onLoad() {
+function hasherOnLoad() {
 
+  get(keysURL, function(request) {
+    let json = JSON.parse(request.responseText)
+    let PSWDSECRET = json['SHA-256-PASSKEY']
+
+    const hashEncryptInput = document.getElementById('hashEncrypt.input')
+    const hashEncryptHash = document.getElementById('hashEncrypt.hash')
+    const hashEncryptEncrypted = document.getElementById('hashEncrypt.encryptedHash')
+    document.getElementById('hashEncrypt').addEventListener('input', function(input) {
+      let string = input.target.value
+
+      let hash = CryptoJS.SHA3(string)
+      let encrypted = CryptoJS.AES.encrypt(hash, PSWDSECRET).toString()
+
+      hashEncryptInput.innerHTML = string
+      hashEncryptHash.innerHTML = hash
+      hashEncryptEncrypted.innerHTML = encrypted
+    })
+
+    const hashDecryptInput = document.getElementById('hashDecrypt.input')
+    const hashDecryptDecryptedHash = document.getElementById('hashDecrypt.hash')
+    document.getElementById('hashDecrypt').addEventListener('input', function(input) {
+      let string = input.target.value
+
+      let hash = CryptoJS.AES.decrypt(string, PSWDSECRET)
+
+      hashDecryptInput.innerHTML = string
+      hashDecryptDecryptedHash.innerHTML = hash
+
+    })
+  })
 }
